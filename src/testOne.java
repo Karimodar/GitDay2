b3c5412d3fdbba5e55091a39463e060eff3edcac
varsi <varsi@LAPTOP-6P3CES8D>
2022-08-02 00:22:50 +0100

    thank you

--- src/testOne.java
+++ src/testOne.java
@@ -2,5 +2,12 @@
 public class testOne {
 public static void main(String[] args) {
 	System.out.println("sdkgwoighj");
+	
+	
+	
 }
+//for US123
+	public static void add(int a, int b) {
+		System.out.println(a+b);
+	}
 }
